6642012577f7e68bc2cc275cc9dbf2402a7b1b55
KhadaGeno <143861134+KhadaGeno@users.noreply.github.com>
2023-11-08 19:31:22 -0300

    Delete TPE directory

--- TPE/app/views/categoria.view.php
+++
@@ -1,9 +0,0 @@
-<?php
-    class categoriaView{
-public function showcategory($categorias){
-        require 'templates/header.phtml';
-        require 'templates/categorias.phtml';
-        require 'templates/footer.phtml';
-    }
-}
-?>
