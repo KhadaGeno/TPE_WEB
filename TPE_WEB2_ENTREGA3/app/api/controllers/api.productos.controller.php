cd440c897d28d5a05acded4d28ee75dd988d68b7
DaniBGA <142959815+DaniBGA@users.noreply.github.com>
2023-11-13 19:28:06 -0300

    Add files via upload

---
+++ TPE_WEB2_ENTREGA3/app/api/controllers/api.productos.controller.php
@@ -0,0 +1,56 @@
+<?php
+require_once './app/api/views/api.view.php';
+require_once './app/api/models/productos.model.php';
+
+class ProductosAPIController {
+
+    private $view;
+    private $model;
+    private $data;
+    
+    function __construct () {
+        $this->view = new ApiView();
+        $this->model = new ProductosModel();
+        $this->data = file_get_contents('php://input');
+    }
+    public function getdata () {
+        return json_decode($this->data);
+    }
+    function getProductos($params = []){
+        if(empty($params)){
+        $sort = 'precio';
+        $order = 'ASC';
+            if(isset($_GET['order'])){
+                $order = $_GET['order'];
+                if($order !='ASC' && $order != 'DESC'){
+                    $order = 'ASC';
+                }
+            }
+            if(isset($_GET['sort'])){
+                $sort = $_GET['sort'];
+                $columnas = array('nombre','descripcion','precio');
+                if(!in_array($sort, $columnas)){
+                $sort = 'precio';
+                }
+            }
+        $productos = $this->model->getProductos($order, $sort);
+        if($productos){
+            $this->view->response($productos,200);
+        }else{
+            $this->view->response("Page not found",404);
+        }
+    }
+    }
+    function getProductosID($params = null){
+        $idProducto = $params[':ID'];
+        $producto = $this->model->getProductosPorId($idProducto);
+        if($producto){
+            $this->view->response($producto,200);
+        }else{
+            $this->view->response("Producto no encontrado",400);
+        }
+    }
+    
+}
+
+    
---
+++ TPE_WEB2_ENTREGA3/routerAPI.php
@@ -0,0 +1,10 @@
+<?php
+    require_once './app/api/libs/router.php';
+    require_once './app/api/controllers/api.productos.controller.php';
+
+$router = new Router();
+
+$router->addRoute('productos','GET','ProductosAPIController','getProductos');
+$router->addRoute('productos/:ID','GET','ProductosAPIController','getProductosID');
+
+$router->route($_REQUEST['resource'], $_SERVER['REQUEST_METHOD']);
